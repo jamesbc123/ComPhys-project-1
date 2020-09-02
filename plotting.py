859ced2e00b4bd5e34d863a7032a59e771084408
jamesbc123 <54575038+jamesbc123@users.noreply.github.com>
2020-09-02 10:19:03 +0200

    corrected plotting of only one legend

--- plotting.py
+++ plotting.py
@@ -7,27 +7,56 @@ Created on Tue Sep  1 12:56:53 2020
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+plt.rcParams.update({'font.size': 22})
+
+plotNumVsSol = False
 
 toi = pd.read_csv("./Results/toi.csv")
-n_schedule = [10, 100, 1000, 10000, int(1e5), int(1e6), int(1e7)]
+n_schedule = np.array([10, 100, 1e3, 1e4, 1e5], dtype=int)
 
 #plot maximum relative error against h.
 plt.figure(figsize=(10,10))
-plt.xlabel("log h")
-plt.ylabel("maximum relative error")
+plt.xlabel("Log(h)")
+plt.ylabel("Maximum Relative Error")
 
+label_added = False
 for n in n_schedule:
     filter_n = toi['n'] == n
-    #max_err_spec = (np.float64(toi[filter_n]['relative error spec'])).max()
-    max_err_gen = (np.float64(toi[filter_n]['relative error gen'])).max()
-    print("general case relative error is", max_err_gen)
-    #plt.scatter(np.log(toi[filter_n]['h'][0]), max_err_spec,
-    #         color = 'r', label ='special')
     h = 1/(n+1)
-    print("h is:", h)
-    print("log10 h :", np.log10(h))
-    plt.scatter(np.log10(h), max_err_gen,
-             color = 'b', label ='general')
+    
+    
+    max_err_spec = (np.float64(toi[filter_n]['relative error spec'])).max()
+    max_err_gen = (np.float64(toi[filter_n]['relative error gen'])).max()
+    
+    if not label_added:
+        plt.scatter(np.log10(h), max_err_spec,
+                 color = 'tab:orange', marker = "^", label ='Special solution')
+        plt.scatter(np.log10(h), max_err_gen,
+                 color = 'b', label ='General solution')
+        label_added = True
+    else:
+        plt.scatter(np.log10(h), max_err_spec,
+                 color = 'tab:orange', marker = "^",)
+        plt.scatter(np.log10(h), max_err_gen,
+                 color = 'b')
 plt.legend()
-plt.savefig("./Results/h_vs_error.png")
+plt.savefig("./Results/error_vs_h.png")
 plt.show()
+
+if plotNumVsSol:
+    # plot x against exact and v
+    for n in n_schedule:
+        plt.figure(figsize=(10,10))
+        plt.xlabel("x")
+        plt.ylabel("numerical or exact solution")
+    
+        filter_n = toi['n'] == n
+        plt.scatter(toi[filter_n]['x'], toi[filter_n]['exact'],
+                 color = 'black', label ='exact')
+        plt.scatter(toi[filter_n]['x'], toi[filter_n]['v general'],
+                 color = 'b', label ='General solution')
+        plt.scatter(toi[filter_n]['x'], toi[filter_n]['v special'],
+                 color = 'r', label ='Special solution')
+        plt.legend()
+        plt.savefig("./Results/solution_vs_exact_n="+ str(n) +".png")
+        plt.close()
