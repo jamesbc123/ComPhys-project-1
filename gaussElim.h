a773f185501394e8b7536acfcf7fe2cedbdd1416
jamesbc123 <54575038+jamesbc123@users.noreply.github.com>
2020-09-05 11:13:07 +0200

    made some corrections
    tried to fix compilation errors such as int a or double a in the functions

--- gaussElim.h
+++ gaussElim.h
@@ -12,7 +12,7 @@ double f(double x) {return 100.0*exp(-10.0*x);}
 
 double exact(double x) {return 1.0-(1-exp(-10))*x-exp(-10*x);}
 
-double* general_algo(int n, double x_0, double x_np1, , a, b, c);
+double* general_algo(int n, double x_0, double x_np1, double a, double b, double c);
 /* Returns solution to general algo and time elapsed.
 This function calculates v for the general case, where
 a, b and c are different values. This is done by forward 
@@ -25,9 +25,31 @@ Outputs:
     v: solution, array of length n+2 */
 
 
-double* special_algo(n, h);
+double* special_algo(int n, double x_0, double x_np1, int a, int b);
+/* Returns solution to the special algorithm and elapsed time.
 
+This function calculates v for the general case, i.e a 
+tri-diagonal matrix, where as opposed to the general case
+a and c are equal and all elements along the diagonal and 
+off-diagonals are identical. 
+Inputs:
+    a, b: integers, diagonal elements
+    n: interger, number of points
+    x_0: double, point 0
+    x_np1: double, point n+1
+Outputs:
+    v: double, solution of size n+1 */
 
+double* relative_error(double v, double u);
+    /* Returns the relative error between v and u.
+    
+    This function compares the exact value to the numerical solution.
+    Inputs:
+        u: an array of the exact values.
+        v: an array of numerical values.
+    Outputs:
+        rel_err: an array of relative errors.
+    */
 
 
 
