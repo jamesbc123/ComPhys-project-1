76346c5b15daf66bf37d9ab773d82dacf25fd2e2
Amund Fredriksen <amund.fredriksen@outlook.com>
2020-09-09 01:35:11 +0200

    Used main.cpp for testing getting the runtimes for the special and general algorithms.

--- main.cpp
+++ main.cpp
@@ -25,7 +25,9 @@ int main() {
     double x_np1 = 1;
 
     // Choose the number of grid points, n:
-    int n = 10;
+    int p = 7; // Power of 10.
+    int n = pow(10, p);
+    cout << "n: " << n << endl;
 
     // Step value:
     double h = (x_np1-x_0)/(n+1);
@@ -49,10 +51,15 @@ int main() {
     b[n-1] = 2; // Fill the last element of b.
 
 
-    double* solution = new double[n+2];
-    general_algo(solution, n, x_0, x_np1, a, b, c);
-    //double* solution = general_algo(n, x_0, x_np1, a, b, c);
-    // ^ 'solution' is an array of length n+2.
+    double* solutionGen = new double[n+2];
+    double* solutionSpec = new double[n+2];
+
+    double tGen = general_algo(solutionGen, n, x_0, x_np1, a, b, c);  // Time elapsed for the 
+    // general algorithm.
+    double tSpec = special_algo(solutionSpec, n, x_0, x_np1, a[0], b[0]); // Time elapsed for the
+    // special algorithm.
+
+    cout << "tGen: " << tGen << ". tSpec: " << tSpec << endl;
 
     // ### STEP 3 ### Choose what values to write to file (what to plot).
     // Choice: Plot the solution as a function of x:
@@ -60,88 +67,23 @@ int main() {
     double* valueList = new double[n+2];
     argumentList = xList;   // The x-axis of the plot.
     valueList = solution;   // The y-axis of the plot. */
-    
-    double* argumentList = xList;
-    double* valueList = solution;
 
-    printArray(xList, n+2);
-    printArray(solution, n+2);
+    //printArray(xList, n+2);
+    //printArray(solution, n+2);
 
     // ### STEP 4 ### Write the solution to a .csv file.
-    string fileName = "data.txt"; // Or 'data.csv'?
+    string fileNameGen = "data_general.txt";
+    string fileNameSpec = "data_special.txt";
     int listLength = n+2;
 
-    writeDataToCSV(argumentList, valueList, listLength, fileName);
+    // Time writing to file.
+    writeDataToCSV(xList, solutionGen, listLength, fileNameGen);
+    writeDataToCSV(xList, solutionSpec, listLength, fileNameSpec);
 
     // Delete all allocated memory:
     delete[] a; delete[] b; delete[] c;
-    delete[] xList; delete[] solution;
+    delete[] xList; delete[] solutionGen; delete[] solutionSpec;
     return 0;
 
     // ### STEP 5 ### In Python: Read the .csv file and plot it.
 }
-
-
-/*
-int main(int argc, char *argv[]){
-  int exponent; 
-    string filename;
-    // We read also the basic name for the output file and the highest power of 10^n we want
-    if( argc <= 1 ){
-          cout << "Bad Usage: " << argv[0] <<
-              " read also file name on same line and max power 10^n" << endl;
-          exit(1);
-    }
-        else{
-        filename = argv[1]; // first command line argument after name of program
-        exponent = atoi(argv[2]);
-    }
-
-    double x_0 = 0;  // Starting point
-    double x_np1 = 1;  // End point (x_(n+1))
-    
-    // Boundary conditions:
-    double u0 = 0; // u_0 = u(x0) = u(0)
-    double u_np1 = 0; // u_(n+1) = u(xn) = u(1)
-    int a = -1;
-    int b = 2;
-
-    // Loop over powers of 10
-    for (int i = 1; i <= exponent; i++){
-      int  n = (int) pow(10.0,i);
-      double h = (x_np1-x_0)/(n+1);
-
-      // Declare new file name
-      string fileout = filename;
-
-      // Convert the power 10^i to a string
-      string argument = to_string(i);
-
-      // Final filename as filename-i-
-      fileout.append(argument);
-      ofile.open(fileout);
-      ofile << setiosflags(ios::showpoint | ios::uppercase);
-      //      ofile << "       x:             approx:          exact:       relative error" << endl;
-      double *v = new double[n+2];
-      double time_special;
-      v_special, time_special = special_algo(n, x_0, x_np1, a , b);
-      double *x = new double[n+2];
-      double *u = new double[n+2];
-      x[0] = x_0; // End points.
-      x[n+1] = x_np1;
-      
-      for (i=1; i<=n; i++){
-          x[i] = x_0 + i*h;
-          u[i] = exact(x[i]);
-      }
-      double *rel_err = new double [n+2];
-      rel_err = relative_error(v_special, u);
-
-      ofile << setw(15) << setprecision(8) << rel_err << endl;
-      
-      ofile.close();
-      
-    }
-    return 0;
-}
-*/
